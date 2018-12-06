7efa287ffccf24f9ea656d4e9247493fea244b01
Adriano Souza <adrianosouza11@gmail.com>
2018-12-05 23:45:01 -0200

    solution with expression lambda declared

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import util.PriceUpdate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Main {
 
@@ -18,7 +19,9 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonstaticPriceUpdate);
+        Consumer<Product> pred = p -> p.setPrice(p.getPrice() * 1.1);
+
+        list.forEach(pred);
 
         list.forEach(System.out::println);
     }
