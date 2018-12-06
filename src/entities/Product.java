faf57ccabfb2d870d1769a505f7a0c12b38156c0
Adriano Souza <adrianosouza11@gmail.com>
2018-12-05 23:43:30 -0200

    solution with refrence method of method nonstatic

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -32,6 +32,10 @@ public class Product {
         p.setPrice(p.getPrice() * 1.1);
     }
 
+    public void nonstaticPriceUpdate(){
+        this.setPrice(price * 1.1);
+    }
+
     @Override
     public String toString() {
         return "Product{" +
