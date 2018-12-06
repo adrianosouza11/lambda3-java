6bcf68efda8b15510323f8fccebd0a91bd632880
Adriano Souza <adrianosouza11@gmail.com>
2018-12-05 23:46:23 -0200

    solution with expression lambda inline

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,10 @@
 package application;
 
 import entities.Product;
-import util.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 public class Main {
 
@@ -19,9 +17,7 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        Consumer<Product> pred = p -> p.setPrice(p.getPrice() * 1.1);
-
-        list.forEach(pred);
+        list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
 
         list.forEach(System.out::println);
     }
